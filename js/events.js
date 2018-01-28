6a912b2fc5e71bce4487ed7965ee9ce04fcaa2b0
David Clark <dlc.clark@gmail.com>
2018-01-28 22:50:12 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -3,11 +3,9 @@
 $(document).ready(function(){
 
 function getIt(){
-  // $('p').on("click", function(){
-  //   alert("Hey!");
-  // });
-  $("p").click(function(){
+  $('p').on("click", function(){
     alert("Hey!");
+  });
 });
 }
 
