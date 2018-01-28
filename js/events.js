baef0531472d3783061af1abf5018b2dcf418c56
David Clark <dlc.clark@gmail.com>
2018-01-28 23:04:16 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -14,7 +14,7 @@ function frameIt(){
 
 function pressIt(){
   $('input').on('keydown', function(key){
-    if(key.which == 47){
+    if(key.which == 71){
       alert("You have pressed the 'g' key.");
     }
   })
